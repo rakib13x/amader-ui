c918a33c38a3a5263f2a1cb3e9f9cfed683cf837
mdrafi276 <mdrafi37473@gmail.com>
2024-02-25 16:28:25 +0600

    navber and banner costomise

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -3,17 +3,24 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-        <div className="md:h-[80vh] text-center opacity-75 grid bg-contain bg-[url('/Banner.jpg')]">
-            <div className="col-start-1 row-start-1 bg-black bg-opacity-20 h-full"></div>
-            <div className="col-start-1 w-screen row-start-1 mx-auto my-auto">
-                <h1 className='text-7xl font-bold mb-7 text-white'>Amader UI</h1>
-                <p className='text-white font-bold
-             text-3xl mb-5 p-5'>Our site features interactive learning modules that allow you to learn at your own pace and in a way that works best for you.</p>
-                <button className='p-5 font-bold mb-5 rounded-full bg-blue-800 text-white border-none'><Link href={'#'}>Let's Explore</Link></button>
-            </div>
+      <div className ="">
+        <div className="md:h-[80vh] mt-5 w-full text-center opacity-75 grid bg-contain bg-[url('https://web.programming-hero.com/home/_next/image?url=%2Fhome%2Flevel2Home%2Fimages%2FwhyThisCourse%2Fbg-right.png&w=640&q=75')]">
+          <div className="col-start-1 row-start-1 bg-black bg-opacity-20 h-full"></div>
+          <div className="col-start-1 w-screen row-start-1 mx-auto my-auto">
+            <h1 className="lg:text-7xl md:text-5xl text-3xl font-bold mb-4 text-white">Amader UI</h1>
+            <p
+              className="text-white font-bold
+             md:text-2xl text-[16px] lg:text-3xl w-[75%] md:w-[60%] mx-auto md:mb-4 p-5"
+            >
+              Our site features interactive learning modules that allow you to
+              learn at your own pace and in a way that works best for you.
+            </p>
+            <button className="p-5 font-bold mb-5 rounded-full bg-blue-800 text-white border-none">
+              <Link href={"#"}>Let's Explore</Link>
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
     );
 };
 
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,38 +6,56 @@ const Navbar = () => {
     const [show, setShow] = useState(false)
     
     return (
-        <div className='flex justify-between px-5 lg:px-10 py-2'>
-            <div>
-                <h1 className='lg:text-2xl font-semibold'>Amader UI</h1>
-            </div>
-            <div>
-                <div className='relative lg:hidden'>
-                    <svg onClick={()=>setShow(!show)} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path strokeinecap="round" strokeLinejoin="round" strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                    <ul className={show ? 'absolute right-0 rounded-lg list-none font-medium text-lg bg-white p-4':'hidden'}>
-                        <Link href={'/'}><li>Home</li></Link>
-                        <li>Components</li>
-                        <li>Docs</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                        
-                    </ul>
-                </div>
-                <div className='hidden lg:block'>
-                    <ul className='list-none font-medium text-lg flex gap-5'>
-                        <li className='active:underline'>Home</li>
-                        <li className='active:underline'>Components</li>
-                        <li className='active:underline'>Docs</li>
-                        <li className='active:underline'>About</li>
-                        <li className='active:underline'>Contact</li>
-                        
-                    </ul>
-                </div>
-            </div>
+      <div className="flex mt-5  justify-between px-5 lg:px-10 py-2">
+        <div>
+          <h1 className="lg:text-2xl font-semibold">Amader UI</h1>
         </div>
+        <div>
+          <div className="relative lg:hidden">
+            <svg
+              onClick={() => setShow(!show)}
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+            <ul
+              className={
+                show
+                  ? "absolute right-0 rounded-lg list-none font-medium text-lg bg-white p-4"
+                  : "hidden"
+              }
+            >
+              <Link href={"/"}>
+                <li>Home</li>
+              </Link>
+              <li className="hover:text-[#e261ff]">Components</li>
+              <li className="hover:text-[#e261ff]">Docs</li>
+              <li className="hover:text-[#e261ff]">About</li>
+              <li className="hover:text-[#e261ff]">Contact</li>
+            </ul>
+          </div>
+          <div className="hidden lg:block">
+            <ul className="list-none font-medium text-lg flex gap-5">
+              <li className="active:underline hover:text-[#e261ff]">Home</li>
+              <li className="active:underline hover:text-[#e261ff]">
+                Components
+              </li>
+              <li className="active:underline hover:text-[#e261ff]">Docs</li>
+              <li className="active:underline hover:text-[#e261ff]">About</li>
+              <li className="active:underline hover:text-[#e261ff]">Contact</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
 };
 
